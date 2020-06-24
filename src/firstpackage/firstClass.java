a504cb7f9e9f3b0512b4220a7e323d1171643e12
Mehmet <eikosan.bal@gmail.com>
2020-06-24 11:07:06 +0300

    Second Repo

--- src/firstpackage/firstClass.java
+++ src/firstpackage/firstClass.java
@@ -4,7 +4,7 @@ public class firstClass {
 
 	public static void main(String[] args) {
 		System.out.println("First Commit from local repository");
-
+		System.out.println("Second commit from local repository");
 	}
 
 }
