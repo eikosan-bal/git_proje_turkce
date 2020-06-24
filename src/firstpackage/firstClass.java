52c7784807cd4b550f558c652047312c10821e64
Mehmet <eikosan.bal@gmail.com>
2020-06-24 11:46:08 +0300

    conflict olusturma

--- src/firstpackage/firstClass.java
+++ src/firstpackage/firstClass.java
@@ -5,7 +5,7 @@ public class firstClass {
 	public static void main(String[] args) {
 		System.out.println("First Commit from local repository");
 		System.out.println("Second commit from local repository");
-		System.out.println("Third commit from local repository");
+		System.out.println("Ucuncu commitimiz local repository deyken");
 		System.out.println("First commit from remote repository");
 	}
 
