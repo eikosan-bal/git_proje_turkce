531ab0727b3b96fce4358f5079ca0a845ecca3d2
Mehmet <eikosan.bal@gmail.com>
2020-06-24 11:12:16 +0300

    Third repo

--- src/firstpackage/firstClass.java
+++ src/firstpackage/firstClass.java
@@ -5,6 +5,7 @@ public class firstClass {
 	public static void main(String[] args) {
 		System.out.println("First Commit from local repository");
 		System.out.println("Second commit from local repository");
+		System.out.println("Third commit from local repository");
 	}
 
 }
